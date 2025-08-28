62163b9a6fe581c1be1a168040c88517c20e0422
Darth Asclepio <falexander2001gmail.com>
2025-08-28 01:48:02 -0500

    Avance 1.0

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = [];
-console.log(amigos)
+let listadoAmigos = [];
 
-function adicionAmigo() {
-    
+
+// Paso 1: Agregamos la funcion para agregar amigos. 
+
+function agregarAmigo() {
+    let añadeAmigos = document.getElementById("amigo");
+    let nombreAmigo = añadeAmigos.value.trim();
+
+// En caso de que el dato de ingreso este en blanco
+    if (nombreAmigo === "") {
+        alert("Por favor ingresa el nomnbre de alguno de tus amigos.")
+        return;
+    } else if (listadoAmigos.includes(nombreAmigo)) {
+        alert("Parece ser que este nombre ya esta en la lista, añade uno diferente.");
+        return;
+    }
+
+    listaAmigos.push(nombreAmigo);
+    añadeAmigos.value = "";
+
+    //console.log(nombreAmigo);
 }
+
